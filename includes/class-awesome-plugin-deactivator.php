e7bc476750557b43a81aed0a433c5f845072d518
YaraElmalah <yaraelmalah2016@gmail.com>
2022-03-07 15:56:58 +0200

    Test Github

---
+++ includes/class-awesome-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://webw.us
+ * @since      1.0.0
+ *
+ * @package    Awesome_Plugin
+ * @subpackage Awesome_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Awesome_Plugin
+ * @subpackage Awesome_Plugin/includes
+ * @author     We Build Websites LLC  <sales@webw.me>
+ */
+class Awesome_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
